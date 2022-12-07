75b3c43c79392d5380464144083df957e9335a6f
Norman Köster <nkoester@itemis.com>
2022-12-07 10:25:37 +0100

    Add npmInstall task dependency in dashboard build

--- spa-dashboard-angular/build.gradle.kts
+++ spa-dashboard-angular/build.gradle.kts
@@ -17,3 +17,5 @@ val npmRun by tasks.creating(com.github.gradle.node.npm.task.NpmTask::class) {
     dependsOn(tasks.getByName("build"))
     args.addAll("run-script", "ng", "serve")
 }
+
+tasks.getByName("build").dependsOn("npmInstall")
