53a755caed41bc2c9205df8b22d88633befdf938
Norman <nkoester@users.noreply.github.com>
2022-12-08 15:48:19 +0100

    Update model-server/build.gradle.kts
    Co-authored-by: Johannes Wienke <jwienke@itemis.com>

--- model-server/build.gradle.kts
+++ model-server/build.gradle.kts
@@ -12,7 +12,7 @@ application {
     mainClass.set("org.modelix.model.server.Main")
     applicationDefaultJvmArgs = listOf("-XX:MaxRAMPercentage=85")
 
-    // well this would be nice. if it worked...
+    // well this would be nice if it was actually supported by the gradle plugin...
     // args = listOf("-inmemory", "-dumpin courses.modelsever.dump")
 }
 
